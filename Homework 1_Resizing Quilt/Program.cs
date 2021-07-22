5a525abd06e644cfe519dac9e7d829112d88ba97
Evangeline Drink <evangelinedrink@gmail.com>
2021-07-22 12:38:05 -0700

    Initial code for lines 1 to 3 in Homework 1: Resizing Quilts

--- Homework 1_Resizing Quilt/Program.cs
+++ Homework 1_Resizing Quilt/Program.cs
@@ -6,6 +6,10 @@ namespace Homework_1_Resizing_Quilt
     {
         static void Main(string[] args)
         {
+            line1();
+            line2();
+            line3();
+
             //Each line of the quilt has its own Method. The Parameter is important because it will determine the number of dots that will be placed in the quilt.
             //Create methods for resizing the quilt (if we don't have a method, the code will run the same the entire time.
             //The For loop looks good for producing the diamonds and periods.
@@ -23,12 +27,115 @@ namespace Homework_1_Resizing_Quilt
             //In the 5th and 6th spaces, create the < (5th space) and > (6th space) parts (maybe a For loop and nest in the If/Else statement for the < and >)
             //Create the 4 dots in spaces 7-10 (Can use a For loop to print out the dots)
             //For the 11th and 12th spaces, create the < (11th space) and the > (12th space)
+            //For lines 13 to 16, create a blank space on the line.
 
             /*For line 3*/
             //Create 2 blank spaces in line (use For loop)
             //In the 3rd and 4th space, create the < and > (I think I can use a For loop for the <> and . ). After creating the diamond, use a nested loop (like another For loop) to create the periods.
             //Create the dots in spaces 5-12 (nested For loop)
-            //
+            //For lines 13 and 14, create the < (line 13) and > (line 14)
+            //For lines 15 and 16, create blank spaces in the line.
+        }
+
+        public static void line1()
+        {
+            //Create 6 blank spaces in line (might use a For loop where initialization corresponds to the position of the space on the line until the 7th space on line)
+            for(int i=1; i<7; i++)
+            {
+                Console.Write(" "); //This states for each space (represented by i), the Console will place an empty space (represented by " ").  We want this on one line, so we use Console.Write instead of Console.WriteLine.
+            }
+
+            //For spaces 7-10, follow the following code (might use a For loop with and If/Else statement to create the < or >)
+            //On the 7th space (and 9th space), insert a <
+            //On the 8th space (and 10th space), insert a >
+            for(int i=7; i<11; i++)
+            {
+                int currentValue = i;
+
+                if((currentValue==7) && (currentValue== 9))
+                {
+                    Console.Write("<");
+                } else
+                {
+                    Console.Write(">");
+                }
+            }
+
+            //Insert 6 additional blank spaces in the line (use a For loop for this. i will correspond to the space on the line (space 11 until the 16th space)
+            for(int i=11; i<17; i++)
+            {
+                Console.Write(" "); //This states for each space (represented by i), the Console will place an empty space (represented by " ").  We want this on one line, so we use Console.Write instead of Console.WriteLine.
+            }
+        }
+
+        public static void line2()
+        {
+            //Creating a new line for the second line pattern
+            Console.WriteLine("\n");
+            
+            //Create 4 blank spaces in line (use For loop)
+            for (int i=1; i<5; i++)
+            {
+                Console.Write(" "); //This states for each space (represented by i), the Console will place an empty space (represented by " ").  We want this on one line, so we use Console.Write instead of Console.WriteLine.
+            }
+
+            //In the 5th and 6th spaces, create the < (5th space) and > (6th space) parts (maybe a For loop and nest in the If/Else statement for the < and >)
+            //Create the 4 dots in spaces 7-10 (Can use a For loop to print out the dots)
+            //For the 11th and 12th spaces, create the < (11th space) and the > (12th space)
+            for (int i=5; i<13; i++)
+            {
+                if((i==5) && (i ==11))
+                {
+                    Console.Write("<");
+                } else if ((i==6) && (i==12))
+                {
+                    Console.Write(">");
+                } else
+                {
+                    Console.Write("."); //For spaces 7 to 10, a period will be placed on the line.
+                }
+            }
+
+            //For lines 13 to 16, create a blank space on the line.
+            for (int i=13; i<17; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        public static void line3()
+        {
+            //Creating a new line for the third line pattern
+            Console.WriteLine("\n");
+
+            //Create 2 blank spaces in line (use For loop)
+            for(int i=1; i<3; i++)
+            {
+                Console.Write(" ");
+            }
+
+            //In the 3rd and 4th space, create the < and > (I think I can use a For loop for the <> and . ). After creating the diamond, use a nested loop (like another For loop) to create the periods.
+            //Create the dots in spaces 5-12 (nested For loop)
+            //For lines 13 and 14, create the < (line 13) and > (line 14)
+            for (int i=3; i<15; i++)
+            {
+                if((i==3) && (i == 13))
+                {
+                    Console.Write("<");
+                } else if((i==4) && (i==14))
+                {
+                    Console.Write(">");
+                } else
+                {
+                    Console.Write(".");  //Creating the dots in spaces 5-12
+                }
+            }
+
+            //For lines 15 and 16, create blank spaces in the line.
+            for(int i=15; i<17; i++)
+            {
+                Console.Write(" ");
+            }
         }
     }
 }
